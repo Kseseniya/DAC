cbe4945f90f6c38d0b2b000ec5066aa3f2687205
Kseseniya <70349987+Kseseniya@users.noreply.github.com>
2021-04-08 16:45:14 +0300

    Add files via upload

---
+++ 3 skript.py
@@ -0,0 +1,78 @@
+#-*-coding:utf-8-*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import RPi.GPIO as GPIO
+import time
+
+num_bits = 8
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(10, GPIO.OUT)
+GPIO.setup(9, GPIO.OUT)
+GPIO.setup(11, GPIO.OUT)
+GPIO.setup(5, GPIO.OUT)
+GPIO.setup(6, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+GPIO.setup(19, GPIO.OUT)
+GPIO.setup(26, GPIO.OUT)
+
+D = [10, 9, 11, 5, 6, 13, 19, 26]
+
+GPIO.output(D[:], 0)
+
+def decToBinList(decNumber):
+    decNumber = decNumber % 256
+    N = num_bits - 1
+    bits = []
+    while N > 0:
+        if(int(decNumber/(2**N)) == 1):
+            bits.append(1)
+            decNumber -= 2**N
+        else:
+            bits.append(0)
+        N -= 1
+    bits.append(decNumber)
+    return bits
+
+def num2dac(value):
+    bits = decToBinList(value)
+    for i in range (0, num_bits):
+        GPIO.output(D[i], bits[num_bits - (i + 1)])
+
+def makesin(time, freq, samplingFrequency):
+	step = 1 / samplingFrequency
+	npoints = int (time * samplingFrequency + 0.5)
+	rads = [0 for i in range(npoints)]
+	cords = [0 for i in range(npoints)]
+	times = [0 for i in range(npoints)]
+	for i in range(0, npoints, 1):
+		rads[i] = freq * step * np.pi * i
+		cords[i] = (int(128 * (math.sin(rads[i]) + 1)))
+		times[i] = step * i
+	plt.plot(times, cords)
+	plt.title('Синусоида')
+	plt.xlabel('Время')
+	plt.ylabel('Значения напряжения')
+	plt.show()
+	return cords
+
+try:
+	times = float(input("Введите время: "))
+	freq = float(input("Введите частоту синусоидального сигнала: "))
+	samplingFrequency = int(input("Введите частоту семплирования: "))
+	samplingPeriod = float(1 / samplingFrequency)
+	if freq <= 0 or times <= 0 or samplingFrequency <= 0:
+		print("Вы ввели некорректное число. Производится выход из программы")
+		exit()
+	ndarray = makesin(times, freq, samplingFrequency)
+	for i in range(0, int(times * samplingFrequency + 0.5), 1):
+		num2dac(ndarray[i])
+		time.sleep(samplingPeriod)
+except KeyboardInterrupt:
+	print("\n")
+	print(" Программа была остановлена пользователем ")
+	print("\n")
+finally:
+	GPIO.cleanup()
---
+++ 4_script.py
@@ -0,0 +1,72 @@
+#-*-coding:utf-8-*-
+
+from os.path import dirname, join as pjoin
+from scipy.io import wavfile
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import scipy.io
+import RPi.GPIO as GPIO
+import time
+
+
+num_bits = 8
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(10, GPIO.OUT)
+GPIO.setup(9, GPIO.OUT)
+GPIO.setup(11, GPIO.OUT)
+GPIO.setup(5, GPIO.OUT)
+GPIO.setup(6, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+GPIO.setup(19, GPIO.OUT)
+GPIO.setup(26, GPIO.OUT)
+
+D = [10, 9, 11, 5, 6, 13, 19, 26]
+
+GPIO.output(D[:], 0)
+
+def decToBinList(decNumber):
+    decNumber = decNumber % 256
+    N = num_bits - 1
+    bits = []
+    while N > 0:
+        if(int(decNumber/(2**N)) == 1):
+            bits.append(1)
+            decNumber -= 2**N
+        else:
+            bits.append(0)
+        N -= 1
+    bits.append(decNumber)
+    return bits
+
+def num2dac(value):
+    bits = decToBinList(value)
+    for i in range (0, num_bits):
+        GPIO.output(D[i], bits[num_bits - (i + 1)])
+
+def makesin(time, freq, samplingFrequency):
+    step = 1 / samplingFrequency
+    npoints = int (time * samplingFrequency)
+    rads = [0 for i in range(npoints)]
+    cords = [0 for i in range(npoints)]
+    times = [0 for i in range(npoints)]
+    for i in range(0, npoints, 1):
+        rads[i] = freq * step * np.pi * i
+        cords[i] = (int(128 * math.sin(rads[i])))
+        times[i] = step * i
+        cords[i] = cords[i] + 128
+    plt.plot(times, cords)
+    plt.title('Синусоида')
+    plt.xlabel('Время')
+    plt.ylabel('Значения напряжения')
+    plt.show()
+    return cords
+
+samplerate, data = wavfile.read("/home/student/Downloads/SOUND.WAV")
+
+print("Частота дискретизации = ", samplerate)
+print(f"Количество каналов = {data.shape[1]}")
+channels = data.shape[1]
+length = float(data.shape[0] / samplerate)
+print(f"Длительность = {length}с")
